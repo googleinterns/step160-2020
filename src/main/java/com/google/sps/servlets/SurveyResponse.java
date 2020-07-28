b7c52a1a8c6252dc513c6e594de1d9ca4da3505a
Peter Tone <peter2parque@gmail.com>
2020-07-28 19:05:02 +0000

    Use AutoValue for SurveyResponse class

--- src/main/java/com/google/sps/servlets/SurveyResponse.java
+++ src/main/java/com/google/sps/servlets/SurveyResponse.java
@@ -1,5 +1,6 @@
 package com.google.sps.servlets;
 
+import com.google.auto.value.AutoValue;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7,8 +8,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/** An immutable data type that represents PANAS survey responses.*/
-public class SurveyResponse {
+/** A value class that represents PANAS survey responses.*/
+@AutoValue
+abstract class SurveyResponse {
 
     /** 
      * Create a SurveyResponse instance representing a single survey response submitted 
@@ -16,43 +18,32 @@ public class SurveyResponse {
      * the message {@code text}, the zipcode {@code zipcode} (provided by the user), and the 
      * feelings and respective intensities indicated on the survey as the map {@code feelings}. 
      */
-    public SurveyResponse(String user, 
+    static SurveyResponse create(String user, 
         Map<PanasFeelings, PanasIntensity> feelings,
         String text,
         String zipcode,
         long timestamp) {
-        throw new UnsupportedOperationException();
+        return new AutoValue_SurveyResponse(user, feelings, text, zipcode, timestamp);
     }
 
     /** Returns the username included in the survey response.*/
-    public String getUser() {
-        throw new UnsupportedOperationException();
-    }
+    abstract String user();
 
     /**
     * Returns an immutable map representing the feelings (and their intensities) included 
     * in the survey response.
     */
-    public Map<PanasFeelings, PanasIntensity> getFeelings() {
-        throw new UnsupportedOperationException();
-    }
+    abstract Map<PanasFeelings, PanasIntensity> feelings();
 
     /** Returns the text included in the survey response.*/
-    public String getText() {
-        throw new UnsupportedOperationException();
-    }
+    abstract String text();
 
     /** Returns the zipcode included in the survey response as a String.*/
-    public String getZipcode() {
-        throw new UnsupportedOperationException();
-    }
+    abstract String zipcode();
 
     /**
     * Returns the timestamp at which the survey response was processed by the server, in 
     * UNIX time.
     */
-    public long getTimestamp() {
-        throw new UnsupportedOperationException();
-    }
-
+    abstract long timestamp();
 }
